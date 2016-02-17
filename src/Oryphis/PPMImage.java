9537e0a009d106ada750622ba688cbf076da6228
Pilloufatou <vincekilleur7@gmail.com>
2016-02-17 12:02:24 +0100

    Commentaire sur le fichier PPMImage

--- src/Oryphis/PPMImage.java
+++ src/Oryphis/PPMImage.java
@@ -20,13 +20,14 @@ public class PPMImage {
 	private Pixel[][] pixels;
 
 	/**
-	 * { constructor_description }
+	 * Constrtucteur d'image PPM
 	 *
-	 * @param      w       { parameter_description }
-	 * @param      h       { parameter_description }
-	 * @param      format  { parameter_description }
+	 * @param      w        largeur de l'image
+	 * @param      h        hauteur de l'image
+	 * @param      format   format de l'image
 	 * 
 	 */
+
 	public PPMImage(int w, int h, String format) {
 		width = w;
 		height = h;
@@ -34,13 +35,27 @@ public class PPMImage {
 		pixels = new Pixel[width][height];
 	}
 
+	/**
+	 * Construteur d'image bis
+	 *
+	 * @param      img    prend une image au format ppm
+	 * 
+	 */
+	
 	public PPMImage(PPMImage img) {
 		this.width = img.getWidth();
 		this.height = img.getHeight();
 		this.format = img.getFormat();
 		this.pixels = new Pixel[width][height];
-		this.pixels = img.getPixels();
+		this.pixels = img.pixelMap();
 	}
+
+	/**
+	 * Permet de récupérer la largeur, la hauteur et les pixels RGB d'une image
+	 *
+	 * @param      path  addresse où se trouve l'image
+	 * 
+	 */
 
 	public PPMImage(String path) 
 	throws java.io.FileNotFoundException, java.io.IOException {
@@ -52,7 +67,15 @@ public class PPMImage {
 		_createPixelMap(in);
 	}
 
-	//Sauvegarde
+	/**
+	 * Permet de sauvegarder une image
+	 *
+	 * @param      path   Adresse ou se trouve l'image
+	 *
+	 * @throws     java   IOException
+	 * 
+	 */
+
 	public void save(String path)
 	throws java.io.FileNotFoundException, java.io.IOException {
 		File file = new File(path);
@@ -64,65 +87,191 @@ public class PPMImage {
 	}
 
 	//Accession pixels
+	
+	/**
+	 * Permet de récupérer les valeurs RGB 
+	 *
+	 * @param      x     coordonnée x du pixel
+	 * @param      y     coordonnée y du pixel
+	 *
+	 * @return     retourne les valeurs RGB du pixel de coordonnée (x;y)
+	 * 
+	 */
+
 	public Pixel pixelAt(int x, int y) {
 		return pixels[x][y];
 	}
+
+	/**
+	 * Permet de retourner tous les pixels
+	 *
+	 * @return     retourne tous les pixels
+	 * 
+	 */
 
 	public Pixel[][] pixelMap() {
 		return pixels;
 	}
 
+	/**
+	 * Permet de récupérer la valeur de la couleur rouge (red)
+	 *
+	 * @param      x     coordonnée x du pixel
+	 * @param      y     coordonnée y du pixel
+	 *
+	 * @return     retourne la valeur rouge (red) de coordonnées (x;y)
+	 * 
+	 */
+
 	public int r(int x, int y) {
 		return pixels[x][y].r;
 	}
 
+	/**
+	 * Permet de récupérer la valeur de la couleur vert (green)
+	 *
+	 * @param      x     coordonnée x du pixel
+	 * @param      y     coordonnée y du pixel
+	 *
+	 * @return     retourne la valeur vert (green) de coordonnées (x;y)
+	 * 
+	 */
+
 	public int g(int x, int y) {
 		return pixels[x][y].g;
 	}
+
+	/**
+	 * Permet de récupérer la valeur de la couleur bleu (blue)
+	 *
+	 * @param      x     coordonnée x du pixel
+	 * @param      y     coordonnée y du pixel
+	 *
+	 * @return     retourne la valeur bleu (blue) de coordonnées (x;y)
+	 * 
+	 */
 
 	public int b(int x, int y) {
 		return pixels[x][y].b;
 	}
 
 	//Manipulation pixels
-	public void setPixel(int x, int y, Pixel p) {
+	
+	/**
+	 * Permet de modifier la valeur RGB du pixel (x;y)
+	 *
+	 * @param      x     coordonnée x du pixel
+	 * @param      y     coordonnée y du pixel
+	 * @param      p     valeur du pixel RGB 
+	 * 
+	 */
+
+	public void setPixel(int x, int y, Pixel p ) {
 		pixels[x][y] = p;
 	}
+
+	/**
+	 * Permet de modifier la valeur rouge (red) du pixel (x;y)
+	 *
+	 * @param      x     coordonnée x du pixel
+	 * @param      y     coordonnée y du pixel
+	 * @param      _r     valeur du pixel rouge (red)
+	 * 
+	 */
 
 	public void r(int x, int y, int _r) {
 		pixels[x][y].r = _r;
 	}
 
+	/**
+	 * Permet de modifier la valeur vert (green) du pixel (x;y)
+	 *
+	 * @param      x     coordonnée x du pixel
+	 * @param      y     coordonnée y du pixel
+	 * @param      _g     valeur du pixel vert (green)
+	 * 
+	 */
+
 	public void g(int x, int y, int _g) {
 		pixels[x][y].g = _g;
 	}
+
+	/**
+	 * Permet de modifier la valeur bleu (blue) du pixel (x;y)
+	 *
+	 * @param      x     coordonnée x du pixel
+	 * @param      y     coordonnée y du pixel
+	 * @param      _b     valeur du pixel bleu (blue)
+	 * 
+	 */
 
 	public void b(int x, int y, int _b) {
 		pixels[x][y].b = _b;
 	}
 
 	//Accesseurs
+	
+	/**
+	 * Permet de récupérer la largeur de l'image
+	 *
+	 * @return     La largeur de l'image
+	 * 
+	 */
+
 	public int getWidth() {
 		return this.width;
 	}
+
+	/**
+	 * Permet de récupérer la hauteur de l'image
+	 *
+	 * @return     La hauteur de l'image
+	 * 
+	 */
 
 	public int getHeight() {
 		return this.height;
 	}
 
+	/**
+	 * Permet de récupérer le format de l'image
+	 *
+	 * @return     Le format de l'image
+	 * 
+	 */
+
 	public String getFormat() {
 		return this.format;
 	}
+
+	/**
+	 * Permet de récupérer la valuer de chaque couleur de coordonnées (x;y)
+	 *
+	 * @param      x     coordonnée x
+	 * @param      y     coordonnée y
+	 *
+	 * @return     la valeur rouge (red), vert (green) et bleu (blue)
+	 */
 
 	public Color colorAt(int x, int y) {
 		return new Color(pixels[x][y].r, pixels[x][y].g, pixels[x][y].b);
 	}
 
-	public Pixel[][] getPixels() {
-		return pixels;
-	}
-
 	//Privés
+	
+	/**
+	 * Permet de récupérer la prochaine ligne sans les espaces et les retours chariot
+	 * des côtés extrêmes droite et gauche		
+	 *
+	 * @param      in    BufferedReader
+	 *
+	 * @return     La prochaine ligne sans les espaces, retour chariot 
+	 * 			   des côtés extrême droite et gauche
+	 *
+	 * @throws     java  IOException
+	 * 
+	 */
+
 	private String _nextLine(BufferedReader in) throws java.io.IOException {
 		String line = in.readLine().trim();
 		while (line.charAt(0) == '#')
@@ -131,9 +280,23 @@ public class PPMImage {
 		return line;
 	}
 
+	/**
+	 * Permet de récupérer la ligne du fichier ou est écrit le format de l'image
+	 *
+	 * @param      line    Une ligne d'un fichier ou est écrit le format de l'image 
+	 * 
+	 */
+
 	private void _getFormat(String line) {
 		this.format = line;
 	}
+
+	/**
+	 * Permet de récupérer les dimensions d'une image
+	 *
+	 * @param      line  Une ligne d'un fichier ou est écrit les dimensions de l'image
+	 * 
+	 */
 
 	private void _getSize(String line) {
 		int pos = line.indexOf(" ");
@@ -143,6 +306,15 @@ public class PPMImage {
 		this.width = Integer.parseInt(lvalue);
 		this.height = Integer.parseInt(rvalue);
 	}
+
+	/**
+	 * Créé la map de pixels à partir d'un BufferedReader (fichier)
+	 *
+	 * @param      in    Le BufferedReader
+	 *
+	 * @throws     java  IOException
+	 * 
+	 */
 
 	private void _createPixelMap(BufferedReader in) throws java.io.IOException {
 		int x = 0;
@@ -161,6 +333,15 @@ public class PPMImage {
 			}
 		}
 	}
+
+	/**
+	 * Permet de sauvegarder la map de pixels à partir d'un BufferedWritter
+	 *
+	 * @param      out   BufferedWritter
+	 *
+	 * @throws     java  IOException
+	 * 
+	 */
 
 	private void _savePixelMap(BufferedWriter out) throws java.io.IOException {
 		out.write(format + "\n");
